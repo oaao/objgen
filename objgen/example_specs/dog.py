eb9cf7322817a6dbd86833fe5a21555d22361e9e
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-19 15:52:04 -0600

    fix [examples]: dog - make the two examples referentially distinct

--- objgen/example_specs/dog.py
+++ objgen/example_specs/dog.py
@@ -5,13 +5,12 @@ DOG_INFO = {
     'actions': {'run_to': 'car', 'bark_at': 'squirrel', 'pee_on': 'hydrant'},
 }
 
-DOG_INFO_FILTERING = DOG_INFO
-DOG_INFO_FILTERING.update({
+DOG_INFO_FILTERING = {
      'friends': {
          'lassie': {'breed': 'collie', 'met_at': 'dog park'},
          'marnie': {'breed': 'shih tzu', 'met_at': 'dms'},
          'air_bud': {'breed': 'golden retriever', 'met_at': 'basketball courts'},
          'laika': {'breed': 'mongrel', 'met_at': 'outer space'}
     }
-})
-
+}
+DOG_INFO_FILTERING.update(DOG_INFO)
